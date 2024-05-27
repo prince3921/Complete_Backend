d7b77382caa21dcb50d7f51d6d22e4de5dde031f
Prince Kumar <pk392160@gmail.com>
2024-05-27 10:15:16 +0530

    MONGODB_CONNECTION

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constaints.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST:${connectionInstance}`);
+        console.log("Connected");
+    } catch (error) {
+        console.log("MONGODB CONNECTION ERROR:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
+
+
+
+
+
+
+
+
+
+
